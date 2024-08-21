2daad3b19f61962b73c416617f36c94c41c3369d
yash <yashsaxena935@gmail.com>
2024-08-21 12:24:49 +0530

    java 26

---
+++ opps1.java
@@ -0,0 +1,14 @@
+public class opps1 {
+    int rollno;
+    String name;
+    String adress;
+    public void show(){
+        System.out.println("name of student is " + name);
+    }
+    public void alldetails(){
+        System.out.println("name of student is " + name );
+        System.out.println("id of student is " + rollno);
+        System.out.println("adress of student is " + adress );
+    }
+    }
+
---
+++ opps2.java
@@ -0,0 +1,11 @@
+public class opps2 {
+    public static void main(String args[]){
+        opps1 st1 = new opps1();
+        st1.adress= "delhi";
+        st1.name= "yash";
+        st1.rollno= 1221333;
+        st1.show();
+        st1.alldetails();
+
+    }
+}
